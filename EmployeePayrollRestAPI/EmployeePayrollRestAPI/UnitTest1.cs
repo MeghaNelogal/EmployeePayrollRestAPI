525b77fca33eaf885b0f1c2d5d89235a89bbb249
Your Name <you@example.com>
2022-09-09 13:23:52 +0530

    [Megha] Add: Retrieve All employee Details

--- EmployeePayrollRestAPI/EmployeePayrollRestAPI/UnitTest1.cs
+++ EmployeePayrollRestAPI/EmployeePayrollRestAPI/UnitTest1.cs
@@ -1,11 +1,50 @@
+using Newtonsoft.Json;
+using RestSharp;
+
 namespace EmployeePayrollRestAPI
 {
-    [TestClass]
-    public class UnitTest1
+    public class Employee
     {
-        [TestMethod]
-        public void TestMethod1()
+        public int id { get; set; }
+        public string name { get; set; }
+        public string salary { get; set; }
+    }
+
+    [TestClass]
+    public class RestSharpCase
+    {
+        RestClient client;
+
+        [TestInitialize]
+        public void Setup()
         {
+            client = new RestClient("http://localhost:4000");
+        }
+
+        private IRestResponse getEmployeeList()
+        {
+            //arrange
+            RestRequest request = new RestRequest("/employees", Method.GET);
+
+            //act
+            IRestResponse response = client.Execute(request);
+            return response;
+        }
+
+        //UC1:- Ability to Retrieve all Employees in EmployeePayroll JSON Server.
+        [TestMethod]
+        public void onCallingGETApi_ReturnEmployeeList()
+        {
+            IRestResponse response = getEmployeeList();
+            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.OK);
+            List<Employee> dataResponse = JsonConvert.DeserializeObject<List<Employee>>(response.Content);
+            Assert.AreEqual(5, dataResponse.Count);
+
+            foreach (Employee e in dataResponse)
+            {
+                Console.WriteLine("id : " + e.id + " , Name: " + e.name + " Salary: " + e.salary);
+            }
         }
     }
+}
 }
